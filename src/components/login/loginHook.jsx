d424006eeb01588b56875ddf8a7c37beb2a421d5
jesuselias <jesus_e1992@hotmail.com>
2020-09-16 10:46:12 -0400

    styled-components

--- src/components/login/loginHook.jsx
+++ src/components/login/loginHook.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+
+import styled from "styled-components"
 import "./style.scss";
 
 import loginImg from "../../logo.svg"
+
+const Button = styled.button`
+    background: white;
+    color: #70ccdb;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #70ccdb;
+    border-radius: 3px;
+    display: block;
+`;
+
+const Input = styled.input`
+    background-color:#d5d5f4;
+    margin-top: 6px;
+    min-width: 18em;
+    height: 37px;
+    padding: 0px 10px;
+    font-size: 16px;
+    font-family: "Open sans", sans-serif;
+    border: 0;
+    border-radius: 4px;
+    margin-bottom: 15px;
+    transition: all 250ms ease-in-out;
+    &:focus {
+        outline: none;
+        box-shadow: opx opx 12px 0.8px blue;
+    }
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #56afc4;
+`;
 
 const LoginHook = () => {
   const { handleSubmit, register, errors } = useForm();
@@ -11,15 +48,14 @@ const LoginHook = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
     <div className="base-container">
-    <div className="header">Login</div>
+    <Title>Login</Title>
     <div className="content">
         <div className="image">
             <img src={loginImg} />
         </div>
         <div className="form">
             <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
+            <Input
             type="text"
             name="username"
             placeholder="username"
@@ -31,11 +67,10 @@ const LoginHook = () => {
                 }
             })}
             />
-            {errors.username && errors.username.message}
+           {  errors.username  && <text style={{color: '#f07171'}}>{ errors.username.message }</text>}
             </div>
             <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
+            <Input
             type="password"
             name="password"
             placeholder="password"
@@ -46,12 +81,10 @@ const LoginHook = () => {
                 }
             })}
             />
-            {errors.password && errors.password.message}
+            {errors.password && <text style={{color: '#f07171'}}>{ errors.password.message }</text>}
             </div>
             <div className="footer">
-                <button type="submit" className="btn">
-                    Login
-                </button>
+            <Button>Normal</Button>
             </div>
         </div>
     </div>
